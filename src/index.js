8be281b1248b6876e41a9d5dc5ee224e3c8178f4
Arthur Manuel Bandeira <arthur.bandeira@objective.com.br>
2019-11-08 17:48:41 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './i18n';
+// import './i18n';
 import App from './components/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { IntlProvider, injectIntl } from 'react-intl';
+
+
+// const App = ({post}) => (
+//   <div>
+//     <h1>{post.title}</h1>
+//     <p>
+//       <PostDate date={post.date} />
+//     </p>
+//     <div>{post.body}</div>
+//   </div>
+// );
+const messages = 
 
 ReactDOM.render(
+  <IntlProvider locale={navigator.language} messages={messages}>
     <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
-    document.getElementById('root')
+      <App />
+    </BrowserRouter>
+  </IntlProvider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
